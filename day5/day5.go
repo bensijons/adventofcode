a5074376e9478b464c1a08f32b9006b70881a731
Benedikt Jónsson <bensijons@gmail.com>
2022-12-08 21:30:09 +0000

    overly complex initialization

--- day5/day5.go
+++ day5/day5.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
 // [D]
 // [N] [C]
@@ -13,17 +18,52 @@ import "fmt"
 // move 1 from 1 to 2
 
 func main() {
+	file, err := os.Open("./day5/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	initializing := true
+	intialStackLines := []string{}
+	crates := map[int][]string{}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			initializing = false
+		} else if initializing {
+			intialStackLines = append(intialStackLines, line)
+		}
+	}
+	initializeStack(crates, intialStackLines)
+
 	stackMap := map[int][]string{}
 	stackMap[1] = []string{"Z", "N", "D"}
 	stackMap[2] = []string{"M", "C"}
 	stackMap[3] = []string{"P"}
 	move(stackMap, 1, 2, 1)
 	for i := 1; i <= len(stackMap); i++ {
-		sl := stackMap[i]
-		fmt.Println(sl[len(sl)-1])
+		// sl := stackMap[i]
+		// fmt.Println(sl[len(sl)-1])
 	}
 
 	fmt.Println("Day 5 part 1 answer is")
+}
+
+func initializeStack(stack map[int][]string, lines []string) {
+	for lineIndex := len(lines) - 2; lineIndex >= 0; lineIndex-- {
+		for crateIndex, k := 1, 1; crateIndex < len(lines[lineIndex]); crateIndex += 4 {
+			crate := string(lines[lineIndex][crateIndex])
+			if crate != " " {
+				crates := stack[k]
+				crates = append(crates, crate)
+				stack[k] = crates
+			}
+			k++
+		}
+	}
 }
 
 func move(stackMap map[int][]string, count int, from int, to int) {
