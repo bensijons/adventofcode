802923157737620d5337c5f2f214de800e4eb6d1
Benedikt Jónsson <bensijons@gmail.com>
2022-12-11 21:26:23 +0000

    fix(d7p1): more explicit folder names
    folders can have the same name so we need add them to the map based on their full path

--- day7/day7.go
+++ day7/day7.go
@@ -22,31 +22,53 @@ func main() {
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		w := strings.Split(line, " ")
 
-		if w[0] == "$" {
-			if w[1] == "cd" && w[2] != ".." {
-				folder := w[2]
-				folders = append(folders, folder)
-			} else if w[1] == "cd" && w[2] == ".." {
-				folders = folders[:len(folders)-1]
-			}
-		} else if w[0] != "dir" {
-			size, err := strconv.Atoi(w[0])
-			if err != nil {
-				log.Fatal(err)
-			}
-			for _, folder := range folders {
-				sizeMap[folder] = sizeMap[folder] + size
-			}
-		}
+		folders = ParseCommands(folders, line, sizeMap)
 	}
 	CalculateTotalSize(sizeMap)
 }
 
+func ParseCommands(folders []string, line string, sizeMap map[string]int) []string {
+	w := strings.Split(line, " ")
+
+	if w[0] == "$" {
+		if w[1] == "cd" && w[2] != ".." {
+			folder := w[2]
+			parentFolder := getParentFolderPath(folders)
+			folders = append(folders, getFolderPath(parentFolder, folder))
+		} else if w[1] == "cd" && w[2] == ".." {
+			folders = folders[:len(folders)-1]
+		}
+	} else if w[0] != "dir" {
+		size, err := strconv.Atoi(w[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, folder := range folders {
+			sizeMap[folder] = sizeMap[folder] + size
+		}
+	}
+	return folders
+}
+
+func getParentFolderPath(folders []string) string {
+	if len(folders) == 0 {
+		return ""
+	}
+
+	return folders[len(folders)-1]
+}
+
+func getFolderPath(parentFolderPath string, folder string) string {
+	if parentFolderPath == "" {
+		return folder
+	}
+	return fmt.Sprintf("%s%s/", parentFolderPath, folder)
+}
+
 func CalculateTotalSize(sizeMap map[string]int) {
 	sum := int(0)
-	for _, folderSize := range sizeMap {
+	for k, folderSize := range sizeMap {
 		if folderSize <= 100000 {
 			sum += folderSize
 		}
