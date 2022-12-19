67c42576f61a392318a1ba7f611654d02ab074a3
Benedikt Jónsson <bensijons@gmail.com>
2022-12-19 23:42:14 +0000

    wip: day 8 part two

--- day8/day8.go
+++ day8/day8.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	file, err := os.Open("./day8/input.txt")
+	file, err := os.Open("./day8/test.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -20,18 +20,22 @@ func main() {
 	}
 
 	visibleTrees := make(map[string]bool)
-	findVisibleTreesLeftAndTop(visibleTrees, lines)
-	findVisibleTreesRightAndDown(visibleTrees, lines)
+	scenicScore := make(map[string]int)
+	findVisibleTreesLeftAndTop(visibleTrees, lines, scenicScore)
+	findVisibleTreesRightAndDown(visibleTrees, lines, scenicScore)
 
 	fmt.Println("Day 8 part 1 answer is:", len(visibleTrees))
+
+	fmt.Println(scenicScore)
 }
 
-func findVisibleTreesLeftAndTop(visibleTrees map[string]bool, lines []string) {
+func findVisibleTreesLeftAndTop(visibleTrees map[string]bool, lines []string, scenicScore map[string]int) {
 	largestTop := make(map[int]int)
 	largestLeft := 0
 	for i := 0; i < len(lines); i++ {
 		line := lines[i]
 		for j, c := range line {
+			key := fmt.Sprintf("%d-%d", i, j)
 			current, _ := strconv.Atoi(string(c))
 			if i == 0 || i == len(lines)-1 {
 				largestTop[j] = current
@@ -44,10 +48,12 @@ func findVisibleTreesLeftAndTop(visibleTrees map[string]bool, lines []string) {
 				continue
 			}
 			if current > largestLeft {
+				calculateScenicScore(scenicScore, key, j)
 				largestLeft = current
 				setVisible(visibleTrees, i, j)
 			}
 			if current > largestTop[j] {
+				calculateScenicScore(scenicScore, key, i)
 				largestTop[j] = current
 				setVisible(visibleTrees, i, j)
 			}
@@ -55,12 +61,18 @@ func findVisibleTreesLeftAndTop(visibleTrees map[string]bool, lines []string) {
 	}
 }
 
-func findVisibleTreesRightAndDown(visibleTrees map[string]bool, lines []string) {
+func findVisibleTreesRightAndDown(visibleTrees map[string]bool, lines []string, scenicScore map[string]int) {
 	largestDown := make(map[int]int)
 	largestRight := 0
+	downCounter := 0
 	for i := len(lines) - 1; i >= 0; i-- {
+		downCounter++
+		rightCounter := 0
 		line := lines[i]
+
 		for j := len(line) - 1; j >= 0; j-- {
+			rightCounter++
+			key := fmt.Sprintf("%d-%d", i, j)
 			current, _ := strconv.Atoi(string(line[j]))
 			if i == 0 || i == len(lines)-1 {
 				largestDown[j] = current
@@ -73,14 +85,25 @@ func findVisibleTreesRightAndDown(visibleTrees map[string]bool, lines []string) 
 				continue
 			}
 			if current > largestRight {
+				calculateScenicScore(scenicScore, key, rightCounter)
 				largestRight = current
 				setVisible(visibleTrees, i, j)
 			}
 			if current > largestDown[j] {
+				calculateScenicScore(scenicScore, key, downCounter)
 				largestDown[j] = current
 				setVisible(visibleTrees, i, j)
 			}
 		}
+	}
+}
+
+func calculateScenicScore(scenicScore map[string]int, key string, multiplier int) {
+	val, ok := scenicScore[key]
+	if ok {
+		scenicScore[key] = val * multiplier
+	} else {
+		scenicScore[key] = multiplier
 	}
 }
 
